12efa6a5692d2cdd09c12babfae09b05a0a5176d
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-25 19:31:33 +0530

    v1.0

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("welcome to TypeScript");
+function myFuntion(str, num) {
+    return str + num;
+}
+var valu = myFuntion("Hello", 10);
+console.log(valu);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
 console.log("welcome to TypeScript");
+
+function myFuntion(str: string,num: number): String{
+    return str+num;
+}
+
+let valu = myFuntion("Hello", 10);
+
+console.log(valu);
