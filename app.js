2cc6e0999fda927e04679534fc2daf632917360f
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-26 13:42:57 +0530

    v1.2

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-console.log("welcome to TypeScript");
-function myFuntion(str, num, val1) {
-    if (val1 === void 0) { val1 = "hey"; }
-    return str + num + val1;
+// console.log("welcome to TypeScript");
+// function myFuntion(str: string,num: number,val1: string="hey"): string{
+//     return str+num+ val1;
+// }
+// let valu = myFuntion("Dinuja",10);
+// console.log(valu);
+function performAction(event) {
+    var _a;
+    var val = document.getElementById("textbox").value;
+    var liTag = document.createElement("li");
+    liTag.innerHTML = val;
+    (_a = document.querySelector(".list")) === null || _a === void 0 ? void 0 : _a.appendChild(liTag);
 }
-var valu = myFuntion("Dinuja", 10);
-console.log(valu);
--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-console.log("welcome to TypeScript");
+// console.log("welcome to TypeScript");
 
-function myFuntion(str: string,num: number,val1: string="hey"): string{
-    return str+num+ val1;
-}
+// function myFuntion(str: string,num: number,val1: string="hey"): string{
+//     return str+num+ val1;
+// }
 
-let valu = myFuntion("Dinuja",10);
+// let valu = myFuntion("Dinuja",10);
 
 
-console.log(valu);
+// console.log(valu);
+
+function performAction(event:Event){
+    const val = (<HTMLInputElement>document.getElementById("textbox")).value;
+    let liTag = document.createElement("li");
+    liTag.innerHTML = val; 
+    document.querySelector(".list")?.appendChild(liTag);
+  }
