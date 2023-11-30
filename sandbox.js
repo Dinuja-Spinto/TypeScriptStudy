08676016057b2afbb87d49da3b0e8a622ebcdf9c
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-30 21:20:38 +0530

    t4 array

--- sandbox.js
+++ sandbox.js
@@ -4,4 +4,12 @@ character = 'Pinto';
 var circ = function (diameter) {
     return diameter * Math.PI;
 };
-console.log(circ(7.5));
+console.log(circ(14));
+//arrays
+var names = ['mario', 'anna', 'yoshi'];
+names.push('test');
+var nums = [2, 3, 4];
+nums.push(29);
+var mixed = ['Ken', 4, 'abc', 10];
+mixed.push('hello');
+mixed.push(18);
--- sandbox.ts
+++ sandbox.ts
@@ -7,4 +7,15 @@ const circ = (diameter: number) =>{
     return diameter * Math.PI;
 }
 
-console.log(circ(7.5));
+console.log(circ(14));
+
+//arrays
+let names = ['mario', 'anna', 'yoshi'];
+names.push('test');
+
+let nums = [2,3,4]
+nums.push(29)
+
+let mixed = ['Ken',4, 'abc', 10]
+mixed.push('hello')
+mixed.push(18)
